20c1fff108fe4b4f3102d2777afa2fa183e984c9
addugger <addugger@gmail.com>
2021-08-24 23:58:49 -0400

    Messing with JPA entities

--- src/main/resources/demoschema.sql
+++ src/main/resources/demoschema.sql
@@ -2,10 +2,10 @@ CREATE SCHEMA IF NOT EXISTS demo_db;
 
 CREATE TABLE IF NOT EXISTS demo_db.demo_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL,
-    last_name VARCHAR(250) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     middle_initial  CHAR,
-    job VARCHAR(250),
+    job VARCHAR(10),
     age SMALLINT NOT NULL,
     doe TIMESTAMP NOT NULL
 );
